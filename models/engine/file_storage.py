3650e8e75312f1a93f219e95dcb3a366418a0edb
BardoftheOzarks <1886@holbertonschool.com>
2020-11-02 17:55:18 +0000

    reload fully functional

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,12 +32,14 @@ class FileStorage:
 
     def reload(self):
         '''loads objects from JSON file'''
+        from models.base_model import BaseModel
+        classes = {'BaseModel': BaseModel}
         try:
-            with open(self.__file_path) as f:
-                undead = json.load(f)
-                for key, value in undead:
+            with open(self.__file_path, 'r') as f:
+                book_of_the_dead = json.load(f)
+                for key, value in book_of_the_dead.items():
                     obj_class = value['__class__']
-                    new_zombie = obj_class(**value)
-                    self.new(new_zombie)
+                    new_zombie = classes[obj_class](**value)
+                    self.__objects.update({key: new_zombie})
         except:
             pass
