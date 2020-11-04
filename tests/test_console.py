ae51574f8d0a18c360060f8c22ec382e5042c3f8
BardoftheOzarks <1886@holbertonschool.com>
2020-11-04 19:37:38 +0000

    added BaseModel tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+'''Tests for the console'''
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    '''Let's put the console to the test'''
+    def test_console(self):
+        
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+'''Unit tests for BaseModel'''
+from unittest import TestCase
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(TestCase):
+    '''Tests BaseModel'''
+    def test_init(self):
+        '''tests init'''
+        base = BaseModel()
+        self.assertIsInstance(base, BaseModel)
+        self.assertIn('id', base.__dict__)
+        self.assertIn('created_at', base.__dict__)
+        self.assertIn('updated_at', base.__dict__)
+        self.assertIsInstance(base.id, str)
+        self.assertIsInstance(base.created_at, datetime)
+        self.assertisInstance(base.updated_at, datetime)
